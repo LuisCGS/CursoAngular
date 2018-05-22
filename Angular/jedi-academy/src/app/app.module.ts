d15b7691976d1166d88d863959a4def2d7641847
Luis Claudio G. Sanches <luis.sanches@sigma.com.br>
2018-05-22 16:53:18 -0300

    Commit do template inicial sistema restaurante

--- Angular/jedi-academy/src/app/app.module.ts
+++ Angular/jedi-academy/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		StudentComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
